45b2f5fe665251338f7b4bbc87bbba6f414e6b96
joacoesperon <joacoesperon1@gmail.com>
2024-02-16 17:08:05 +0100

    nivel 1 listo

--- bloques.c
+++ bloques.c
@@ -3,7 +3,9 @@
 // Variable global estática para el descriptor del fichero
 static int descriptor = 0;
 
-int bmount(const char *camino){ //Montar el dispositivo virtual
+int bmount(const char *camino)
+{ // Montar el dispositivo virtual
+    umask(000);
     descriptor = open(camino, O_RDWR | O_CREAT, 0666);
     if (descriptor == -1)
     {
@@ -14,8 +16,10 @@ int bmount(const char *camino){ //Montar el dispositivo virtual
     return descriptor;
 }
 
-int bumount(){ //Desmonta el dispositivo virtual
-    if (close(descriptor) == -1) {
+int bumount()
+{ // Desmonta el dispositivo virtual
+    if (close(descriptor) == -1)
+    {
         // Error al cerrar el fichero
         perror(RED "Error en bumount al cerrar el fichero" RESET);
         return FALLO;
@@ -23,33 +27,39 @@ int bumount(){ //Desmonta el dispositivo virtual
     return EXITO;
 }
 
-int bwrite(unsigned int nbloque, const void *buf){
+int bwrite(unsigned int nbloque, const void *buf)
+{
     off_t desplazamiento = nbloque * BLOCKSIZE;
     // O ASI    off_t puntero =lseek(descriptor, desplazamiento, SEEK_SET);
-    if (lseek(descriptor, desplazamiento, SEEK_SET) == -1) {
+    if (lseek(descriptor, desplazamiento, SEEK_SET) == -1)
+    {
         fprintf(stderr, "%sError en bwrite al mover el puntero del fichero: %s%s\n", RED, strerror(errno), RESET);
         return FALLO;
     }
 
     size_t bytes_escritos = write(descriptor, buf, BLOCKSIZE);
-    if (bytes_escritos == -1) {
+    if (bytes_escritos == -1)
+    {
         fprintf(stderr, "%sError en bwrite al escribir en el fichero: %s%s\n", RED, strerror(errno), RESET);
         return FALLO;
     }
 
     return bytes_escritos;
-}  
+}
 
-int bread(unsigned int nbloque, void *buf) {
+int bread(unsigned int nbloque, void *buf)
+{
     off_t desplazamiento = nbloque * BLOCKSIZE;
 
-    if (lseek(descriptor, desplazamiento, SEEK_SET) == -1) {
+    if (lseek(descriptor, desplazamiento, SEEK_SET) == -1)
+    {
         fprintf(stderr, "%sError en bread al mover el puntero del fichero: %s%s\n", RED, strerror(errno), RESET);
         return FALLO;
     }
 
     size_t bytes_leidos = read(descriptor, buf, BLOCKSIZE);
-    if (bytes_leidos == -1) {
+    if (bytes_leidos == -1)
+    {
         fprintf(stderr, "%sError en bread al leer del fichero: %s%s\n", RED, strerror(errno), RESET);
         return FALLO;
     }
--- mi_mkfs.c
+++ mi_mkfs.c
@@ -1,7 +1,9 @@
 #include "bloques.h"
 
-int main(int argc, char **argv) {
-    if (argc != 3) {
+int main(int argc, char **argv)
+{
+    if (argc != 3)
+    {
         fprintf(stderr, "Uso: %s <nombre_dispositivo> <nbloques>\n", argv[0]);
         exit(FALLO);
     }
@@ -10,7 +12,8 @@ int main(int argc, char **argv) {
     unsigned int nbloques = atoi(argv[2]);
 
     int descriptor = bmount(nombre_dispositivo);
-    if (descriptor == FALLO) {
+    if (descriptor == FALLO)
+    {
         fprintf(stderr, "Error al montar el dispositivo virtual.\n");
         exit(FALLO);
     }
@@ -18,15 +21,18 @@ int main(int argc, char **argv) {
     unsigned char buffer[BLOCKSIZE];
     memset(buffer, 0, BLOCKSIZE); // Inicializar el buffer a 0s
 
-    for (unsigned int i = 0; i < nbloques; i++) {
-        if (bwrite(i, buffer) == FALLO) {
+    for (unsigned int i = 0; i < nbloques; i++)
+    {
+        if (bwrite(i, buffer) == FALLO)
+        {
             fprintf(stderr, "Error al escribir en el bloque %u.\n", i);
             bumount();
             exit(FALLO);
         }
     }
 
-    if (bumount() == FALLO) {
+    if (bumount() == FALLO)
+    {
         fprintf(stderr, "Error al desmontar el dispositivo virtual.\n");
         exit(FALLO);
     }
