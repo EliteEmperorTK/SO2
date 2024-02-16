2a0ddf540cad41114d32618cfb834f558c716d1e
joacoesperon <joacoesperon1@gmail.com>
2024-02-16 12:24:54 +0100

    HOLA

--- bloques.c
+++ bloques.c
@@ -1,1 +1,13 @@
+#include "bloques.h"
 
+int bmount(const char *camino)
+{
+    int descriptor = open(camino, O_RDWR | O_CREAT, 0666);
+    if (descriptor == -1)
+    {
+        // Error al abrir el fichero
+        perror(RED "Error en bmount al abrir el fichero " RESET);
+        return FALLO;
+    }
+    return descriptor;
+}
---
+++ mi_mkfs.c
@@ -0,0 +1,7 @@
+#include "bloques.h"
+
+int main(int argc, char **argv)
+{
+
+    return 0;
+}
