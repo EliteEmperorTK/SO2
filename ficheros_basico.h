7616cdd5fc44eafcb852990adaf5fb06bf816b22
EliteEmperorTK <147103748+EliteEmperorTK@users.noreply.github.com>
2024-03-06 17:09:40 +0100

    Update ficheros_basico.h

--- ficheros_basico.h
+++ ficheros_basico.h
@@ -75,3 +75,5 @@ int liberar_bloque(unsigned int nbloque);
 int escribir_inodo(unsigned int ninodo, struct inodo *inodo);
 int leer_inodo(unsigned int ninodo, struct inodo *inodo);
 int reservar_inodo(unsigned char tipo, unsigned char permisos);
+
+int traducir_bloque_inodo(struct inodo *inodo, unsigned int nblogico, unsigned char reservar)
